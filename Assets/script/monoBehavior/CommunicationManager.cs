9779d36835926e7d5c84f880f4d85b0d3e7ede07
sentendro <ghu333@hanmail.net>
2017-08-15 02:20:50 +0900

    create scripts and images

--- Assets/script/monoBehavior/CommunicationManager.cs
+++ Assets/script/monoBehavior/CommunicationManager.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CommunicationManager : MonoBehaviour {
-
+    GameObject GM;
 	// Use this for initialization
 	void Start () {
-		
+        GM = this.gameObject.GetComponent<Transform>().parent.gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    
 }
---
+++ Assets/script/monoBehavior/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    STATUS_GAME SG;
+    dialog current;
+	// Use this for initialization
+	void Start () {
+        SG = STATUS_GAME.MENU;
+        current = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnGUI()
+    {
+        if(this.SG == STATUS_GAME.DIALOGING)
+        {
+            if (this.current == null)
+                this.SG = STATUS_GAME.PAUSE;
+            else
+            {
+
+            }
+        }
+    }
+    private void setCurrent(dialog dialogs)
+    {
+        this.current = dialogs;
+    }
+}
