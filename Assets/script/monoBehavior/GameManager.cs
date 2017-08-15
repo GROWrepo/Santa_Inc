c3cb7083b92dde7c63427dc9f71b53dd0e7c2a80
sentendro <ghu333@hanmail.net>
2017-08-15 22:10:59 +0900

    implement 10 object search clicked just in case

--- Assets/script/monoBehavior/GameManager.cs
+++ Assets/script/monoBehavior/GameManager.cs
@@ -7,23 +7,35 @@ public class GameManager : MonoBehaviour {
     public GameObject selectedObject;
     dialog current;
     Ray2D clickRay;
-    RaycastHit2D hit;
+    //RaycastHit2D hit;
+    RaycastHit2D[] hits;
+    public ContactFilter2D filter;
 
     // Use this for initialization
     void Start () {
         SG = STATUS_GAME.MENU;
         current = null;
+        hits = new RaycastHit2D[10];
+        filter = new ContactFilter2D();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         if (Input.GetMouseButtonDown(0))
         {
             clickRay = new Ray2D(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)), Vector2.zero);
-            hit = Physics2D.Raycast(clickRay.origin, clickRay.direction);
-            if(hit.collider != null)
+            Physics2D.Raycast(clickRay.origin, clickRay.direction, filter, hits);
+            /*hit = Physics2D.Raycast(clickRay.origin, clickRay.direction, filter, hits);
+            if (hit.collider != null)
             {
-                Debug.Log(hit.transform.gameObject);
+                //Debug.Log(hit.transform.gameObject);
+                //selectedObject = hits[0].transform.gameObject;
+            }*/
+            if(hits[0].collider != null)
+            {
+                Debug.Log(hits[0].transform.gameObject);
+                selectedObject = hits[0].transform.gameObject;
             }
         }
 	}
