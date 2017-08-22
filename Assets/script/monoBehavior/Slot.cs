df430006ef96397919a0ef3ef35684c1dd40aeec
tkfkdqldb1 <tkfkdqldb1@naver.com>
2017-08-22 18:30:31 +0900

    드래그 앤 드랍 중간점검

--- Assets/script/monoBehavior/Slot.cs
+++ Assets/script/monoBehavior/Slot.cs
@@ -1,10 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
-
-public class Slot : MonoBehaviour, IDragHandler
+public class Slot : MonoBehaviour, IDragHandler, IPointerEnterHandler, IPointerExitHandler, IEndDragHandler
 {
 
     public int number;
@@ -13,9 +10,33 @@ public class Slot : MonoBehaviour, IDragHandler
     public void OnDrag(PointerEventData data)
     {
         if (transform.childCount > 0)
-            transform.GetChild(0).parent = Inventory.instance.draggingItem;
+            transform.GetChild(0).SetParent(Inventory.instance.draggingItem);
         Inventory.instance.draggingItem.GetChild(0).position = data.position;
     }
 
-}
+    public void OnPointerEnter(PointerEventData data)
+    {
+        Inventory.instance.enteredSlot = this;
+    }
 
+    public void OnPointerExit(PointerEventData data)
+    {
+        Inventory.instance.enteredSlot = null;
+    }
+
+    public void OnEndDrag(PointerEventData data)
+    {
+        Inventory.instance.draggingItem.GetChild(0).SetParent(transform);
+        transform.GetChild(0).localPosition = Vector3.zero;
+
+        if (Inventory.instance.enteredSlot != null)
+        {
+            Item tempItem = item;
+            item = Inventory.instance.enteredSlot.item;
+            Inventory.instance.enteredSlot.item = tempItem;
+            Inventory.instance.ItemImageChange(this);
+            Inventory.instance.ItemImageChange(Inventory.instance.enteredSlot);
+        }
+
+    }
+}
