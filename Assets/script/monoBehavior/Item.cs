6c22edac0c2d14e18dcfca6711831744b44d2070
tkfkdqldb1 <tkfkdqldb1@naver.com>
2017-08-25 18:42:39 +0900

    new inventory 시작

---
+++ Assets/script/monoBehavior/Item.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item : MonoBehaviour {
+    public string itemName;
+    public int itemID;
+    public string itemDesc;
+    public Texture2D itemIcon;
+    public int itemPower;
+    public int itemSpeed;
+    public ItemType itemType;
+
+    public enum ItemType
+    {
+        Weapon,
+        Consumble,
+        Quest
+    }
+	
+}
