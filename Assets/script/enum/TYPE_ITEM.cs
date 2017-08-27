573c265dd405a6056e915aaa92e73a9546d68f17
sentendro <ghu333@hanmail.net>
2017-08-28 01:49:09 +0900

    update TYPE_ITEM enum

--- Assets/script/enum/TYPE_ITEM.cs
+++ Assets/script/enum/TYPE_ITEM.cs
@@ -2,5 +2,8 @@
 using System.Collections.Generic;
 
 public enum TYPE_ITEM {
-    EXPENDABLES
+    EXPENDABLES,//소모품 - 직접 사용가능하며 갯수를 표시해야함
+    INGREDIENTS,//재료 - 사실상 기타? 카테고리
+    COMPONENTS,//부품 - 일종의 퀘스트 아이템?
+
 }
