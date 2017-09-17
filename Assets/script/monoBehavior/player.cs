c5c655b9baabd94fd32f5a056b83ec64296e27b1
castlesu <shsj4952@naver.com>
2017-09-18 00:28:47 +0900

    player jump,move

--- Assets/script/monoBehavior/player.cs
+++ Assets/script/monoBehavior/player.cs
@@ -2,60 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour {
-    public float movePower;
-    public float jumpPower = 10.0f;
+public class player : MonoBehaviour
+{
+    public float movePower = 1f;
+    public float jumpPower = 1f;
 
     Rigidbody2D rigid;
 
     Vector3 movement;
     bool isJump = false;
 
-     
-
-
-    // Use this for initialization      
-    void Start () {
+    void Start()
+    {
         rigid = gameObject.GetComponent<Rigidbody2D>();
-        movePower = 5.0f;
     }
-
-    // Update is called once per frame
-    
     void Update()
     {
-        Move();
-        Jump();
         if (Input.GetButtonDown("Jump"))
         {
             isJump = true;
         }
-    }
-    void FixedUpdate()
-    {
-    }
-    void Move ()
-    {
-        Vector3 moveVelocity = Vector3.zero;
-        if (Input.GetKey(KeyCode.RightArrow))
         {
-            moveVelocity = Vector3.right;
+            Vector3 moveVelocity = Vector3.zero;
+
+            if (Input.GetAxisRaw("Horizontal") < 0 )
+            {
+                moveVelocity = Vector3.left;
+            }
+            else if (Input.GetAxisRaw("Horizontal") > 0 )
+            {
+                moveVelocity = Vector3.right;
+            }
+
+            transform.position += moveVelocity * movePower * Time.deltaTime;
+
         }
-        else if(Input.GetKey(KeyCode.LeftArrow))
         {
-            moveVelocity = Vector3.left;
+            if (!isJump)
+                return;
+            rigid.velocity = Vector2.zero;
+
+            Vector2 jumpVelocity = new Vector2(0, jumpPower);
+            rigid.AddForce(jumpVelocity, ForceMode2D.Impulse);
+
+            isJump = false;
         }
-        transform.position += moveVelocity * movePower * Time.deltaTime;
     }
-    void Jump ()
-    {
-        if (!isJump)
-            return;
-        rigid.velocity = Vector2.zero;
 
-        Vector2 jumpVelocity = new Vector2(0, jumpPower);
-        rigid.AddForce(jumpVelocity, ForceMode2D.Impulse);
-
-        isJump = false;
-    }
+    
 }
+
+  
