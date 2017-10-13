aa6a4beb6f1a35842af9bd81d5e2893c27a71524
castlesu <shsj4952@naver.com>
2017-10-13 19:53:21 +0900

    restore removed player script

--- Assets/script/monoBehavior/player.cs
+++ Assets/script/monoBehavior/player.cs
@@ -3,14 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour {
+    float speed;
+    bool isUp;
+    bool isRight;
+
+    Rigidbody2D rigid;
+    Vector3 movement;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+        isRight = false;
+        isUp = false;
+
+        speed = 5.0f;
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+		if(Input.GetKey(KeyCode.A))
+        {
+            this.gameObject.GetComponent<Transform>().Translate(-this.speed * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            this.gameObject.GetComponent<Transform>().Translate(this.speed * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.W))
+        {
+            if(this.isUp)
+            {
+                isUp = true;
+                
+           }
+        }
+
 	}
 }
