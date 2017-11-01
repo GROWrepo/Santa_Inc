3da7fd25d1bda963c122688eb334fa1ac5fb6e5e
sentendro <ghu333@hanmail.net>
2017-11-01 19:22:26 +0900

    add script for maincamera & add finish wall for stages

---
+++ Assets/script/monoBehavior/playerCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCamera : MonoBehaviour {
+    GameObject player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        updatePosition();
+	}
+
+    void updatePosition()
+    {
+        this.transform.Translate(new Vector2(player.transform.position.x - this.transform.position.x, player.transform.position.y - this.transform.position.y));
+    }
+}
