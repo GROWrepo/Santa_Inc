baed584b53bf38a6ad156f4c8ee6a70790a6b04f
sentendro <ghu333@hanmail.net>
2017-11-03 19:58:07 +0900

    update playercamera but still not working

--- Assets/script/monoBehavior/playerCamera.cs
+++ Assets/script/monoBehavior/playerCamera.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class playerCamera : MonoBehaviour {
-    GameObject player;
+    public GameObject player;
     Transform[] landscapes;
     Transform[] ends;
+
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
         landscapes = GameObject.Find("LandScapeManager").transform.GetComponentsInChildren<Transform>();
-        ends = landscapes[0].GetComponentsInChildren<Transform>();
+        ends = landscapes[1].GetComponentsInChildren<Transform>();
 	}
 	
 	// Update is called once per frame
@@ -20,11 +21,12 @@ public class playerCamera : MonoBehaviour {
 
     void updatePosition()
     {
-        if (player.transform.position.x >= (ends[0].position.x + 8.4) && player.transform.position.x <= (ends[1].position.x - 8.4))
+        Debug.Log(ends[1]);
+        if (player.transform.position.x >= (ends[1].position.x + 8.4) && player.transform.position.x <= (ends[2].position.x - 8.4))
         {
             this.transform.Translate(new Vector2(player.transform.position.x - this.transform.position.x, 0));
         }
-        if (player.transform.position.y >= (landscapes[1].position.y + 4.5))
+        if (player.transform.position.y >= (landscapes[2].position.y + 4.5))
         {
             this.transform.Translate(new Vector2(0, player.transform.position.y - this.transform.position.y));
         }
