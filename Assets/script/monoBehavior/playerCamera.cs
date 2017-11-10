c797ab7da98adde47d0b4c65f342b1101f4fdb97
sentendro <ghu333@hanmail.net>
2017-11-10 18:35:00 +0900

    complete camera movement to x

--- Assets/script/monoBehavior/playerCamera.cs
+++ Assets/script/monoBehavior/playerCamera.cs
@@ -5,30 +5,42 @@ using UnityEngine;
 public class playerCamera : MonoBehaviour {
     public GameObject player;
     Transform[] landscapes;
-    Transform[] ends;
+    Transform leftEnd;
+    Transform rightEnd;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
         landscapes = GameObject.Find("LandScapeManager").transform.GetComponentsInChildren<Transform>();
-        ends = landscapes[1].GetComponentsInChildren<Transform>();
-	}
+        leftEnd = landscapes[1].GetChild(0);
+        rightEnd = landscapes[1].GetChild(1);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        updatePosition();
+        if (player.transform.position.x >= (leftEnd.position.x + 8.4) && player.transform.position.x <= (rightEnd.position.x - 8.4))
+        {
+            Debug.Log("update");
+            this.transform.Translate(new Vector3(player.transform.position.x - this.transform.position.x, 0, 0));
+        }
+        if (player.transform.position.y >= (landscapes[2].position.y + 4.5))
+        {
+            Debug.Log("update");
+            this.transform.Translate(new Vector3(0, player.transform.position.y - this.transform.position.y), 0);
+        }
 	}
 
     void updatePosition()
     {
-        Debug.Log(ends[1]);
-        if (player.transform.position.x >= (ends[1].position.x + 8.4) && player.transform.position.x <= (ends[2].position.x - 8.4))
+        if (player.transform.position.x >= (leftEnd.position.x + 8.4) && player.transform.position.x <= (rightEnd.position.x - 8.4))
         {
-            this.transform.Translate(new Vector2(player.transform.position.x - this.transform.position.x, 0));
+            Debug.Log(player.transform.position.x);
+            this.transform.Translate(new Vector3(player.transform.position.x - this.transform.position.x, 0, 0));
         }
         if (player.transform.position.y >= (landscapes[2].position.y + 4.5))
         {
-            this.transform.Translate(new Vector2(0, player.transform.position.y - this.transform.position.y));
+            Debug.Log("update");
+            this.transform.Translate(new Vector3(0, player.transform.position.y - this.transform.position.y), 0);
         }
     }
 }
